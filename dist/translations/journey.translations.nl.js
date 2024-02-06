0150dcedccab18d2f97839f6702db14e75fcbe47
William Troup <williamtroup@gmail.com>
2024-02-06 16:10:47 +0000

    Added the base translation files.

---
+++ dist/translations/journey.translations.nl.js
@@ -0,0 +1,8 @@
+/*! Journey.js v1.1.0 - Dutch | (c) Bunoon 2024 | MIT License */
+$journey.setConfiguration( {
+        "backButtonText": "Back",
+        "nextButtonText": "Next",
+        "finishButtonText": "Finish",
+        "closeButtonToolTipText": "Close",
+        "doNotShowAgainText": "Do not show again"
+} );
