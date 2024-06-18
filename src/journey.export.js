2184f686d77dd9c5c57dad6992cb998997bd7751
William Troup <williamtroup@gmail.com>
2024-06-18 14:25:08 +0100

    Added export support for the global "$journey" object, which can now be imported as "journey.js".

---
+++ src/journey.export.js
@@ -0,0 +1,21 @@
+/**
+ * Journey.js
+ * 
+ * A lightweight, easy-to-use JavaScript library to create interactive, customizable, accessible guided tours across your websites or web apps!
+ * 
+ * @file        journey.js
+ * @version     v1.7.2
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2024
+ */
+
+var journey = {
+    js: function() {
+        return window.$journey;
+    }
+};
+
+Object.assign( window, { journey } );
+
+export { journey };
