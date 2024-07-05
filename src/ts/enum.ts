3f9a86f498375fbfb4630ee55048b2eb7f5df78c
William Troup <williamtroup@gmail.com>
2024-07-05 11:10:42 +0100

    Added comments.

--- src/ts/enum.ts
+++ src/ts/enum.ts
@@ -1,3 +1,16 @@
+/**
+ * Journey.js
+ * 
+ * A lightweight, easy-to-use JavaScript library to create interactive, customizable, accessible guided tours across your websites or web apps!
+ * 
+ * @file        journey.js
+ * @version     v2.0.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2024
+ */
+
+
 export const enum Char {
     empty = "",
     space = " ",
