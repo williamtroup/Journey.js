2b85171ab82217050bef1e5c4956cb96e1aa4209
William Troup <williamtroup@gmail.com>
2024-07-06 14:05:05 +0100

    Updated file headers.

--- src/ts/dom.ts
+++ src/ts/dom.ts
@@ -3,7 +3,7 @@
  * 
  * A lightweight, easy-to-use JavaScript library to create interactive, customizable, accessible guided tours across your websites or web apps!
  * 
- * @file        journey.js
+ * @file        dom.ts
  * @version     v2.0.1
  * @author      Bunoon
  * @license     MIT License
